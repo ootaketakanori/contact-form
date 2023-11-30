bede581dcdbce30f36fe3aab72eb7140fab94f7c
ootake takanori <mine19876293@gmail.com>
2023-11-30 10:52:18 +0900

    0-5

--- src/app/Http/Controllers/ContactController.php
+++ src/app/Http/Controllers/ContactController.php
@@ -3,12 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Contact;
 
 class ContactController extends Controller
 {
     public function confirm(Request $request)
     {
         $contact = $request->only(['name', 'email', 'tel', 'content']);
-        return $contact;
+        return view('confirm', ['contact' => $contact]);
+    }
+    public function index()
+    {
+        return view('index');
+    }
+    public function store(Request $request)
+    {
+        $contact = $request->only(['name', 'email', 'tel', 'content']);
+        Contact::create($contact);
+        return view('thanks');
     }
 }
